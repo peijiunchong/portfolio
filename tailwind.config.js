fcbe785c5f761edee0739c2fe2f85d11f7a2f82c
Chong Pei Jiun <52816591+peijiunchong@users.noreply.github.com>
2022-12-11 09:33:08 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"class",
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        butons: "burtons",
+      }
+    },
+  },
+  plugins: [],
+}
